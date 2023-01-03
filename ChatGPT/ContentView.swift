4b35f89b49bc133b8c7dfdbe9c9982c0ddfd950f
Martinus Iudex <martin.richter.1990@gmail.com>
2023-01-03 10:03:09 +0100

    UI Changes

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -7,82 +7,92 @@
 
 import SwiftUI
 import CoreData
+import OpenAISwift
 
+private var apiKey = "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"
+
+final class ViewModel: ObservableObject {
+    init() {
+    }
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: apiKey)
+    }
+    
+    func send(text: String,
+              completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+            
+        })
+    }
+}
+
+
+//MARK: ContentView
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        
+        VStack(alignment: .leading) {
+            
+            
+            ForEach(models, id: \.self) {string in
+                Text(string)
+                    .bold()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            
+            Spacer()
+            
+            HStack {
+                TextField("Frage Alfred...", text: $text)
+                Button("Senden") {
+                    send()
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
+        .onAppear {
+            viewModel.setup()
+        }
+        .padding(.bottom, 0.2 )
+        .padding(.leading)
+        .padding(.trailing)
+        .padding(.top)
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Ich:\n \(text)\n")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                self.models.append("Alfred: "+response)
+                self.text = ""
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
